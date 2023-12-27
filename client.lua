494d38c03f4c5699a780dd847533c07abfd39a64
morohoro <154812080+morohoro@users.noreply.github.com>
2023-12-27 23:52:20 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -9,6 +9,24 @@ local isCalling = false
 local currentCall = nil
 local currentCash = 0
 
+function KeyboardInput(textEntry, inputText, maxLength)
+    AddTextEntry('FMMC_KEY_TIP1', textEntry)
+    DisplayOnscreenKeyboard(1, 'FMMC_KEY_TIP1', '', inputText, '', '', '', maxLength)
+
+    while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+        Citizen.Wait(0)
+    end
+
+    if UpdateOnscreenKeyboard() ~= 2 then
+        local result = GetOnscreenKeyboardResult()
+        Citizen.Wait(500)
+        return result
+    else
+        Citizen.Wait(500)
+        return nil
+    end
+end
+
 RegisterNetEvent('phonebooth:receiveCall')
 AddEventHandler('phonebooth:receiveCall', function(caller, number, duration)
     local playerPed = PlayerPedId()
@@ -39,7 +57,7 @@ Citizen.CreateThread(function()
             if IsPedInAnyVehicle(ped) and QBCore.Functions.GetPedInVehicleSeat(QBCore.Functions.GetVehiclePedIsIn(ped), -1) == ped then
                 DrawText3Ds(pbCoords.x, pbCoords.y, pbCoords.z + 1.2, "Exit Vehicle To Use Phone Booth")
             else
-                DrawText3Ds(pbCoords.x, pbCoords.y, pbCoords.z + 1.5, "Press ~g~E~w~ To Use Phone Booth")
+                DrawText3Ds(pbObj.x, pbObj.y, pbObj.z + 1.2, "Exit Vehicle To Use Phone Booth")
 
                 if IsControlJustReleased(0, 38) then
                     if currentCash > 0 then
@@ -103,13 +121,14 @@ function CreateMenu(menuData, isNearPB)
             elseif data.current.value == "mecano" then
                 menu.Close()
                 TriggerServerEvent('phonebooth:makecall', 'mecano')
-            elseif data.current.value == "othersms" then data.current.value = "othercall"
+            elseif data.current.value == "othersms" then
                 local otherNumber = KeyboardInput("Enter Phone Number:", "", 10)
                 TriggerServerEvent('phonebooth:makecall', otherNumber)
             end
         end, function(data, menu)
             menu.Close()
         end)
+    end
 end
 
 function PhonePlayCall(isCalling)
